138192d882f36a6253d00352baddb67d6ecd9938
Johan Sjölin <johan.sjolin@unomaly.com>
2016-10-10 20:50:30 +0200

    Fix PEP8 compatibility

--- olearyspicker.py
+++ olearyspicker.py
@@ -6,7 +6,8 @@ import argparse
 
 
 class restaurant(object):
-    def __init__(self, title, short_title, slug, address, distance, phone, url):
+    def __init__(
+        self, title, short_title, slug, address, distance, phone, url):
         self.title = title
         self.short_title = short_title
         self.slug = slug
@@ -18,16 +19,20 @@ class restaurant(object):
 
 def main():
     parser = argparse.ArgumentParser(
-        description="Find and randomize nearby O'learys restaurants. Stockholm central coordinates is 18.0686,59.3293. Gothenburg central is 11.9746,57.7089, I hate argparse formatting"
+        description="Find and randomize nearby O'learys restaurants. \
+                     Stockholm central coordinates is 18.0686,59.3293. \
+                     Gothenburg central is 11.9746,57.7089"
     )
     parser.add_argument('-d', '--distance',
                         type=int, default='4000',
                         help='Integer for distance in meters'
                         )
-    parser.add_argument('-c', '--coordinates',
-                        default='18.0686,59.3293',
-                        help='Coordinates in format {format} (W/E, N/S)'.format(format='18.0388702,59.3213309')
-                        )
+    parser.add_argument(
+        '-c', '--coordinates',
+        default='18.0686,59.3293',
+        help='Coordinates in format {format} (W/E, N/S)'.format(
+            format='18.0388702,59.3213309')
+    )
     args = parser.parse_args()
 
     if args.distance is not None:
@@ -39,16 +44,19 @@ def main():
         coordinates = args.coordinates
     else:
         coordinates = '18.0388702,59.3213309'
-
-    r = requests.get(
-        "http://olearys.se/api/v1/restaurants/?language_code=sv-se&site_id=2&point={coordinates}&dist={dist}".format(coordinates=coordinates, dist=distance))
+    
+    api = "http://olearys.se/api/v1/restaurants/?language_code=sv-se&site_id=2"
+    r = requests.get("{api}&point={coordinates}&dist={dist}".format(
+        api=api, coordinates=coordinates, dist=distance))
 
     restaurants = []
 
     data = json.loads(r.text)
     if data['count'] >= 1:
         for line in json.loads(r.text)['results']:
-            restaurants.append(restaurant(line['title'], line['short_title'], line['slug'], line['address'], line['distance'], line['phone'], line['url']))
+            restaurants.append(restaurant(
+                line['title'], line['short_title'], line['slug'],
+                line['address'], line['distance'], line['phone'], line['url']))
 
         rand_rest = restaurants[random.randrange(len(restaurants))]
 

691bfdc05973df8bf037b348c69d40240e8aa18e
Richard Holmboe <richard@holmboe.org>
2016-10-10 19:14:56 +0200

    Added argparse support and also pep8 formatted the stuff. python 2.7 and 3.5 validated.

--- olearyspicker.py
+++ olearyspicker.py
@@ -2,6 +2,7 @@
 import json
 import random
 import requests
+import argparse
 
 
 class restaurant(object):
@@ -16,20 +17,47 @@ class restaurant(object):
 
 
 def main():
-    distance = "4000"
-    point = "18.0388702%2C59.3213309"
-    r = requests.get("http://olearys.se/api/v1/restaurants/?language_code=sv-se&site_id=2&point={point}&dist={dist}".format(point=point, dist=distance))
+    parser = argparse.ArgumentParser(
+        description="Find and randomize nearby O'learys restaurants. Stockholm central coordinates is 18.0686,59.3293. Gothenburg central is 11.9746,57.7089, I hate argparse formatting"
+    )
+    parser.add_argument('-d', '--distance',
+                        type=int, default='4000',
+                        help='Integer for distance in meters'
+                        )
+    parser.add_argument('-c', '--coordinates',
+                        default='18.0686,59.3293',
+                        help='Coordinates in format {format} (W/E, N/S)'.format(format='18.0388702,59.3213309')
+                        )
+    args = parser.parse_args()
+
+    if args.distance is not None:
+        distance = args.distance
+    else:
+        distance = '4000'
+
+    if args.coordinates is not None:
+        coordinates = args.coordinates
+    else:
+        coordinates = '18.0388702,59.3213309'
+
+    r = requests.get(
+        "http://olearys.se/api/v1/restaurants/?language_code=sv-se&site_id=2&point={coordinates}&dist={dist}".format(coordinates=coordinates, dist=distance))
 
     restaurants = []
 
-    for line in json.loads(r.text)['results']:
-        restaurants.append(restaurant(line['title'], line['short_title'], line['slug'], line['address'], line['distance'], line['phone'], line['url']))
+    data = json.loads(r.text)
+    if data['count'] >= 1:
+        for line in json.loads(r.text)['results']:
+            restaurants.append(restaurant(line['title'], line['short_title'], line['slug'], line['address'], line['distance'], line['phone'], line['url']))
 
-    rand_rest = restaurants[random.randrange(len(restaurants))]
+        rand_rest = restaurants[random.randrange(len(restaurants))]
 
-    print rand_rest.title
-    print rand_rest.address
-    print rand_rest.phone
+        print(rand_rest.title)
+        print(rand_rest.address)
+        print(rand_rest.phone)
+    else:
+        print("No restaurants found within that range and coordinates")
+        print(args)
 
 
 if __name__ == "__main__":
